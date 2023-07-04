20b27ef45bbc8cc3692f261362deeb6db9fc919b
YoonJoon <yoonjoon.lee@gmail.com>
2023-07-04 21:45:22 +0900

    Created django application and customized article model

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+# blog/admin.py
+
+from django.contrib import admin
+from .models import Article
+
+# Register your models here.
+# admin.site.register(Article)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    pass
