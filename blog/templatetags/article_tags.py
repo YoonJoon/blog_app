5f132278918952973c1f09bdb467f43fe725ff73
YoonJoon <yoonjoon.lee@gmail.com>
2023-07-08 16:55:44 +0900

    Displayed latest articles

---
+++ blog/templatetags/article_tags.py
@@ -0,0 +1,17 @@
+# blog/templatetags/article_tags.py
+
+from django import template
+from ..models import Article
+
+register = template.Library()
+
+@register.simple_tag
+def total_articles():
+    return Article.publishedArticles.count()
+    pass
+
+@register.inclusion_tag('blog/latest_articles.html')
+def show_latest_articles(count=3):
+    latest_articles = Article.publishedArticles.order_by('-publish')[:count]
+    return {'latest_articles': latest_articles}
+    pass
